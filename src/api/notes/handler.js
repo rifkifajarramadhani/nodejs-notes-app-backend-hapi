02f7be650026ff24c157d8189a0c5bda33fc9bc5
rifkifajarramadhani <rifkid56@gmail.com>
2024-02-26 11:20:25 +0700

    implement database with postgres

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -10,11 +10,11 @@ class NotesHandler {
         this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
     }
 
-    postNoteHandler(req, h) {
+    async postNoteHandler(req, h) {
         this._validator.validateNotePayload(req.payload);
         const { title = 'untitled', body, tags } = req.payload;
 
-        const noteId = this._service.addNote({ title, body, tags });
+        const noteId = await this._service.addNote({ title, body, tags });
 
         const res = h.response({
             status: 'success',
@@ -27,8 +27,8 @@ class NotesHandler {
         return res;
     }
 
-    getNotesHandler() {
-        const notes = this._service.getNotes();
+    async getNotesHandler() {
+        const notes = await this._service.getNotes();
         return {
             status: 'success',
             data: {
@@ -37,9 +37,9 @@ class NotesHandler {
         }
     }
 
-    getNoteByIdHandler(req, h) {
+    async getNoteByIdHandler(req, h) {
         const { id } = req.params;
-        const note = this._service.getNoteById(id);
+        const note = await this._service.getNoteById(id);
         return {
             status: 'success',
             data: {
@@ -48,11 +48,11 @@ class NotesHandler {
         };
     }
 
-    putNoteByIdHandler(req, h) {
+    async putNoteByIdHandler(req, h) {
         this._validator.validateNotePayload(req.payload);
         const { id } = req.params;
 
-        this._service.editNoteById(id, req.payload);
+        await this._service.editNoteById(id, req.payload);
 
         return {
             status: 'success',
@@ -60,10 +60,10 @@ class NotesHandler {
         }
     }
 
-    deleteNoteByIdHandler(req, h) {
+    async deleteNoteByIdHandler(req, h) {
         const { id } = req.params;
 
-        this._service.deleteNoteById(id);
+        await this._service.deleteNoteById(id);
 
         return {
             status: 'success',
